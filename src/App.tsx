ceb4604e221d2ed900e2aa6f00b65942e8d31e59
Meghana Vasanthi <130841184+Vasanthi0812@users.noreply.github.com>
2025-03-05 01:23:30 +0530

    tryfooter

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import Footer from "./components/ui/Footer";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import WeeklyPlanner from "./pages/WeeklyPlanner";
 import NotFound from "./pages/NotFound";
-
 const queryClient = new QueryClient();
 
 const App = () => (
@@ -14,6 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
+      </Footer>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<WeeklyPlanner />} />
@@ -22,6 +23,7 @@ const App = () => (
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
+
     </TooltipProvider>
   </QueryClientProvider>
 );
